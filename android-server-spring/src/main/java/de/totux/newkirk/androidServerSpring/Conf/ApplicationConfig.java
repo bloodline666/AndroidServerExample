9c8439025efbc1181dee050ca5c78aa8f07fa683
bloodline666 <a.daschner@totux.de>
2014-10-10 12:13:12 +0200

    Configs

--- android-server-spring/src/main/java/de/totux/newkirk/androidServerSpring/Conf/ApplicationConfig.java
+++ android-server-spring/src/main/java/de/totux/newkirk/androidServerSpring/Conf/ApplicationConfig.java
@@ -1,28 +1,93 @@
 package de.totux.newkirk.androidServerSpring.Conf;
 
-import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages={"de.totux.newkirk.androidServerSpring"})
 @EnableTransactionManagement
+@PropertySources(@PropertySource("classpath:database.properties"))
 public class ApplicationConfig {
 	
-	@Bean(name = "dataSource")
-	public DataSource getDataSource() {
-	    BasicDataSource dataSource = new BasicDataSource();
-	    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	    dataSource.setUrl("jdbc:mysql://localhost:3306/usersdb");
-	    dataSource.setUsername("root");
-	    dataSource.setPassword("secret");
-	 
-	    return dataSource;
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = 
+				new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driver"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		return dataSource;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory =
+				new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan("de.totux.newkirk.androidServerSpring");
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		hibernateProperties.put("hibernate.show_sql", true);
+		hibernateProperties.put("hibernate.hbm2ddl.auto", "");
+		sessionFactory.setHibernateProperties(hibernateProperties);
+		return sessionFactory;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager =
+				new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory);
+		return transactionManager;
+	}
+	
+	@Bean 
+	@Autowired
+	public HibernateTemplate hibernateTemplate(SessionFactory sessionFactory) {
+		HibernateTemplate hibernateTemplate = 
+				new HibernateTemplate(sessionFactory);
+		return hibernateTemplate;
+	}
+	
+	@Bean
+	public RequestMappingHandlerAdapter mappingHandlerAdapter() {
+		RequestMappingHandlerAdapter adapter = 
+				new RequestMappingHandlerAdapter();
+		List<HttpMessageConverter<?>> list = new ArrayList<HttpMessageConverter<?>>();
+		list.add(jsonMessageConverter());
+		adapter.setMessageConverters(list);
+		return adapter;
+	}
+	
+	@Bean 
+	public MappingJackson2HttpMessageConverter jsonMessageConverter() {
+		MappingJackson2HttpMessageConverter converter = 
+				new MappingJackson2HttpMessageConverter();
+		return converter;
 	}
 }
--- android-server-spring/src/main/java/de/totux/newkirk/androidServerSpring/Conf/SpringMvcInitializer.java
+++ android-server-spring/src/main/java/de/totux/newkirk/androidServerSpring/Conf/SpringMvcInitializer.java
@@ -6,7 +6,7 @@ public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServ
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return null;
+		return new Class<?>[]{ApplicationConfig.class};
 	}
 
 	@Override
